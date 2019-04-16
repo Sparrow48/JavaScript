5056994e76a6ddfbe1b4b8515b4cfcd3eadc768d
Nasib48 <abunasib48@gmail.com>
2019-04-16 15:14:20 +0600

    Complete functions and methods part

--- 04.Objects and Functions  of js/script.js
+++ 04.Objects and Functions  of js/script.js
@@ -1,3 +1,4 @@
+/*
 var Nasib = {
     firstName : 'Abu',
     lastName : 'Nasib',
@@ -29,4 +30,100 @@ person.prototype.nationality = 'Bangladeshi';
 
 Imran.calculateAge();
 console.log(Imran.nationality);
-console.log(Nasib.nationality);
+console.log(Nasib.nationality); */
+
+// object.creat
+
+/*var personProto = {
+    calculateAge : function (){
+        console.log(2019 - this.calculateAge);
+
+    }
+};
+
+var Nasib = Object.create(personProto);
+
+Nasib.name = 'Nasib';
+Nasib.yearOfBirth = 1995;
+Nasib.job = 'student';
+
+var Imran = Object.create(personProto, {
+    name : { value : 'Imran'},
+    yearOfBirth : { value : 2000},
+    job : { value : 'student'}
+
+});
+
+
+var marks = 94;
+
+var Nasib = {
+    name :'Nasib',
+    department : 'ECE',
+    major : 'CSE'
+};
+
+console.log(marks);
+console.log(Nasib.major);
+
+function change(a,b){
+    a=95;
+    b.major ='ETE';
+}
+
+change(marks,Nasib);
+
+console.log(marks);
+console.log(Nasib.major);
+
+var marks = [88,87,75,86,79];
+
+function shortMarks(arr , fun){
+    var arrayResult = [];
+
+    for(var i=0; i< arr.length ; i++){
+        arrayResult.push(fun(arr[i]));
+    }
+
+    return arrayResult;
+
+}
+
+function shortFromA(mark){
+    return 93 - mark;
+}
+
+var result = shortMarks(marks,shortFromA);
+console.log(result);*/
+
+
+(function(){
+    var score = Math.random() * 10;
+    console.log(score);
+    console.log(score>= 5);
+
+})();
+
+(function(sub){
+    var score = Math.random() * 10;
+    console.log(score);
+    console.log(score>= 5-sub);
+
+})(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
