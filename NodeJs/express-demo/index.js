bfa11cfec14a3fb88d2729a00d6d0f43cf7d813a
Nasib <abu.nasib@northsouth.edu>
2019-06-18 19:41:34 +0600

    Building RESTful API_s Using Express completed

--- NodeJs/express-demo/index.js
+++ NodeJs/express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -29,13 +30,23 @@ app.get('/api/gotit', (req,res) =>{
     res.send(['Nasib','Abir','Nayeem']);
 });
 
+app.get('/api/courses',(req,res) =>{
+    res.send(courses);
+});
+
+
 app.get('/api/courses/:id',(req,res) =>{
     const course = courses.find(c=> c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The given id was not found!');
+    if(!course) return res.status(404).send('The given id was not found!');
     res.send(course);
 });
 
 app.post('/api/course', (req,res)=>{
+
+    const { error } = validateCourse(req.body);
+
+    if(error) return res.status(400).send(result.details[0].message);
+
     const course ={
         id : courses.length +1,
         name: req.body.name
@@ -45,6 +56,43 @@ app.post('/api/course', (req,res)=>{
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id', (req,res)=>{
+
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The given id was not found!');
+
+    const { error } = validateCourse(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+
+});
+
+function validateCourse(course){
+    const schema = {
+        name : Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+};
+
+app.delete('/api/courses/:id', (req,res)=>{
+
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The given id was not found!');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+
+})
+
 
 const port = process.env.PORT || 3000;
 
