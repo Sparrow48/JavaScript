cf0d467ff18a1bd8683cac92933de0813362eb1b
Nasib <abu.nasib@northsouth.edu>
2019-06-21 12:03:32 +0600

    running promise

--- NodeJs/async-demo/index.js
+++ NodeJs/async-demo/index.js
@@ -1,25 +1,46 @@
-console.log('Before');
- getUser(1, (user) =>{
-     console.log('user', user);
+// console.log('Before');
+//  getUser(1, (user) =>{
+//   getTherepo(user.gitUsername, (repos)=>{
+//         getCommits(repo, (commit)=>{
+//             console.log(commits);
+//         });
+//      });
+//  });
 
-     getTherepo(user.gitUsername, (repos)=>{
-         console.log('Repos', repos);
+getUser(1)
+ .then(user => getTherepo(user.gitUsername))
+ .then(repos => getCommits(repos[0]))
+ .then(commits => console.log('Commits',commits))
+ .catch(err => console.log('Error', err.message));
 
-     });
- });
-console.log('After');
+ 
+ console.log('After');
 
-function getUser(id,callback){
-    setTimeout(()=>{
-        console.log('Reading data......');
-        callback({ id: id , gitUsername: 'nasib'});
-    }, 2000);
+
+function getUser(id){
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log('Reading data......');
+            resolve ({ id: id , gitUsername: 'nasib'});
+        }, 2000);
+    });
 };
 
 
-function getTherepo(username,callback){
-    setTimeout(()=>{
-        console.log('calling git api......');
-        callback['repo1','repo2'];
-    }, 2000);
+function getTherepo(username){
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            console.log('calling git api 1......');
+            resolve['repo1','repo2'];
+        }, 2000);
+    });
+};
+
+function getCommits(repo){
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            console.log('calling git api......');
+            resolve(['commit']); 
+        }, 2000);
+    });
 };
