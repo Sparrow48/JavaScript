8ab9f21c4d529215f2238aa60e23738a7da151cd
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-19 09:41:22 +0600

    error handling

--- NodeJs/node-rest-shop/api/routes/orders.js
+++ NodeJs/node-rest-shop/api/routes/orders.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req,res,next) =>{
-    res.status(200).json({
-        message: 'Orders werw fetches'
-    });
-});
-
-router.post('/', (req,res,next) =>{
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Orders was created'
+        message: 'Orders was created',
+        createOrder: order
     });
 });
 
--- NodeJs/node-rest-shop/app.js
+++ NodeJs/node-rest-shop/app.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-allow-Origin","*");
+    res.header("Access-Control-allow-Header","*");
+
+    if(res.method == 'OPTIONS'){
+        res.header('Access-Control-allow-Methods','PUT,POST,PATCH,DELETE.GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) =>{
+    const error = new Error('NOT FOUND');
+    error.status = 404;
+    next(error);
+
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+ 
 module.exports = app;
